3dbb624921e424ad4370ca2cca254aa296451648
Sayani Pathak <sayanipathak01@gmail.com>
2025-08-04 17:42:51 +0100

    hi

---
+++ robot.py
@@ -0,0 +1,30 @@
+from sbot import *
+from sbot import leds, Colour
+from sbot import arduino
+from sbot import comp
+from sbot import motors, utils
+import numpy
+# from sbot import GPIOPinMode
+
+### "is_competition" in metadata.json started true
+
+
+# print(comp.is_competition)
+
+print("hi")
+# while True:
+#     motors.set_power(0, 0.5)
+#     motors.set_power(1, 0.5)
+#     utils.sleep(3)
+
+#     motors.set_power(0, 0)
+#     motors.set_power(1, 0)
+#     utils.sleep(1.4)
+
+#     motors.set_power(0, -0.5)
+#     motors.set_power(1, -0.5)
+#     utils.sleep(1)
+
+#     motors.set_power(0, 0)
+#     motors.set_power(1, 0)
+#     utils.sleep(4)
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 from sbot import *
+
+print(1)
